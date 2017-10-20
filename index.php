4fbc6c336313db9e8950131eccf83226661c7a09
sureshsugan <32965342+sureshsugan@users.noreply.github.com>
2017-10-20 14:11:53 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <title>Hello Cloud Gurus</title>
+    </head>
+
+    <body>
+<div align="center">
+                <h1>Hello Cloud Guru's</h1>
+                <h2>Congratulations! You have successfully installed an application to OpsWorks!</h2>
+                <div align="center"><img src="https://s3.amazonaws.com/acloudguru-opsworkslab-donotdelete/logo-on-black-800.png"></div>
+                <p>You ACloudGuru Application is now running on the host &ldquo;<?php echo gethostname(); ?>&rdquo; in a new stack and layer in OpsWorks.</p>
+                <p>This host is running PHP version <?php echo phpversion(); ?>.</p>
+        </div>
+    </body>
+
+</html>
